a0cb80b01a7cdcd59395fac0a9ff2906bdf5a568
Arie Blumenzweig <theblumz@gmail.com>
2024-12-28 11:23:20 +0200

    moved tasks to common/targets

--- control/acquisitions.py
+++ control/acquisitions.py
@@ -1,4 +1,4 @@
-from planning.tasks import Task
+from common.targets import TaskModel
 from common.utils import path_maker, Activities, RepeatTimer
 from enum import IntFlag, auto
 import logging
@@ -16,7 +16,7 @@ class Acquisition(Activities):
     An Acquisition starts AFTER the @plan was checked and found runnable
     """
 
-    def __init__(self, plan: Task):
+    def __init__(self, plan: TaskModel):
         Activities.__init__(self)
         self.folder = path_maker
         self._logger = logging.getLogger('acquisition')
--- planning/acquisitions.py
+++
@@ -1,8 +0,0 @@
-from planning.targets import Target
-from typing import List
-
-
-class Acquisition:
-
-    def __init__(self):
-        self.targets: List[Target] = []
